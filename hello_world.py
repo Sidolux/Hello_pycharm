698315d2cd13b2d779ac2b5258340f4ed9c2b4b2
Sidolux <midas78@gmail.com>
2018-08-27 18:22:37 +0200

    v1

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,4 @@
 print('Hello World test')
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print("iteration {iteration} is {name}".format(iteration=i, name=name))
